5ae41e5a2505c57a1c6891279e993a0c38b8c2c1
Nishant <nishraut7@gmail.com>
2024-12-18 11:39:02 +0530

    backend url changed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3200'
-        : 'https://pharmacare-be.onrender.com',
+        : 'https://pharmacare-be1.onrender.com',
 });
 
 export default axiosInstance;
